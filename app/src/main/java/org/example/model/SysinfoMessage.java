7c0c9e16322746092813a59d09bcc5e3b67a61b6
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2026-01-22 18:42:50 +0000

    feat: SysinfoMessage

---
+++ app/src/main/java/org/example/model/SysinfoMessage.java
@@ -0,0 +1,32 @@
+package org.example.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Map;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "sysinfo_messages")
+public class SysinfoMessage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JsonProperty("mac_address")
+    private String macAddress;
+    @JsonProperty("read_time")
+    private long readTime;
+
+    @ElementCollection
+    @CollectionTable(name = "sysinfo_metrics", joinColumns = @JoinColumn(name = "message_id"))
+    @MapKeyColumn(name = "metric_name")
+    @Column(name = "metric_value")
+    @JsonProperty("metrics")
+    private Map<String, Double> metrics;
+}
