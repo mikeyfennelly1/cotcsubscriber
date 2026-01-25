1d7b8f7ff4421f0f3b8d3882de7c8a8dd4683ae0
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2026-01-25 23:42:21 +0000

    feat: SysinfoEventSubscriber listens to topic and saves messages to Postgres

---
+++ app/src/main/java/org/example/service/SysinfoEventSubscriber.java
@@ -0,0 +1,36 @@
+package org.example.service;
+
+import io.nats.client.Dispatcher;
+import jakarta.annotation.PostConstruct;
+import org.example.config.NatsConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import io.nats.client.Connection;
+
+import java.nio.charset.StandardCharsets;
+
+@Service
+public class SysinfoEventSubscriber {
+    private final Logger log = LoggerFactory.getLogger(SysinfoEventSubscriber.class);
+    private final Connection natsConnection;
+    private static final String SUBJECT = "desktop-sysinfo";
+
+    @Autowired
+    SysinfoEventSubscriber(NatsConfiguration natsConnection) {
+        this.natsConnection = natsConnection.getConnection();
+    }
+
+    @PostConstruct
+    public void subscribe() {
+        Dispatcher dispatcher = natsConnection.createDispatcher(message -> {
+            String payload = new String(message.getData(), StandardCharsets.UTF_8);
+            log.info("Received message on [{}]: {}", SUBJECT, payload);
+        });
+
+        dispatcher.subscribe(SUBJECT);
+
+        log.info("Subscribed to NATS subject [{}]", SUBJECT);
+    }
+}
