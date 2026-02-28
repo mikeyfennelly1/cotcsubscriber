ddad69f79917fa4015496fb6b3517e592dad2d30
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2026-02-28 20:15:49 +0000

    refactor: gut TimeSeriesRepository & extend from TimeSeriesMessage

--- app/src/main/java/org/example/consumer/repository/TimeseriesRepository.java
+++ app/src/main/java/org/example/consumer/repository/TimeseriesRepository.java
@@ -1,20 +1,9 @@
 package org.example.consumer.repository;
 
-import org.example.consumer.model.device.SysinfoMessage;
+import org.example.consumer.model.TimeSeriesMessage;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.data.jpa.repository.Query;
-import java.util.List;
 
 @Repository
-public interface TimeseriesRepository extends JpaRepository<SysinfoMessage, Long> {
-
-    @Query("SELECT DISTINCT s.macAddress FROM SysinfoMessage s")
-    List<String> findDistinctMacAddresses();
-
-    List<SysinfoMessage> findByMacAddress(String macAddress);
-
-    List<SysinfoMessage> findByMacAddressAndReadTimeBetween(String macAddress, long startTime, long endTime);
-
-    boolean existsByMacAddress(String macAddress);
+public interface TimeseriesRepository extends JpaRepository<TimeSeriesMessage, Long> {
 }
