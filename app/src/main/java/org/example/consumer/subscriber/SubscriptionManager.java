b2254a5cd9167ab05b7d4b6f0a407f0ccaeeeead
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2026-03-01 18:29:12 +0000

    refactor: create subscriber.manager package

--- app/src/main/java/org/example/consumer/subscriber/SubscriptionManager.java
+++
@@ -1,9 +0,0 @@
-package org.example.consumer.subscriber;
-
-import java.util.List;
-
-public interface SubscriptionManager {
-    void createSubscription(String name, String parent) throws TreePathNotFoundException, InvalidSubscriptionTreePathFormatException, SubscriptionAlreadyExistsException;
-    void deleteSubscription(String subscriptionName);
-    List<String> readAllSubscriptions();
-}
--- app/src/main/java/org/example/consumer/subscriber/manager/NatsConnectionSingleton.java
+++ app/src/main/java/org/example/consumer/subscriber/manager/NatsConnectionSingleton.java
@@ -1,4 +1,4 @@
-package org.example.consumer.subscriber;
+package org.example.consumer.subscriber.manager;
 
 import io.nats.client.Connection;
 import io.nats.client.Nats;
@@ -16,7 +16,7 @@ import java.io.IOException;
 import java.time.Duration;
 
 @Component
-class NatsConnectionSingleton {
+public class NatsConnectionSingleton {
     private final Logger logger = LoggerFactory.getLogger(NatsConnectionSingleton.class);
     private final int natsPort;
 
