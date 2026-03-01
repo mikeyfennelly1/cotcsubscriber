294bddaf7ebef9e98522b422bf7bdefb7c2f826e
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2026-03-01 13:11:00 +0000

    refactor: rename CRUD methods in SubscriptionManager

--- app/src/main/java/org/example/consumer/subscriber/SubscriptionManager.java
+++ app/src/main/java/org/example/consumer/subscriber/SubscriptionManager.java
@@ -3,7 +3,7 @@ package org.example.consumer.subscriber;
 import java.util.List;
 
 public interface SubscriptionManager {
-    void newSubscription(String name, String parent) throws TreePathNotFoundException, InvalidSubscriptionTreePathFormatException, SubscriptionAlreadyExistsException;
-    void removeSubscription(String subscriptionName);
-    List<String> getActiveSubscriptions();
+    void createSubscription(String name, String parent) throws TreePathNotFoundException, InvalidSubscriptionTreePathFormatException, SubscriptionAlreadyExistsException;
+    void deleteSubscription(String subscriptionName);
+    List<String> readAllSubscriptions();
 }
