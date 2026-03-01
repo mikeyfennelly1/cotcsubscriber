c25e6b8db8027e02cffdd558f6ed73d38fa92d83
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2026-03-01 12:03:03 +0000

    refactor: timeseries repository to extend time series record entity

--- app/src/main/java/org/example/consumer/repository/TimeseriesRepository.java
+++ app/src/main/java/org/example/consumer/repository/TimeseriesRepository.java
@@ -1,9 +1,9 @@
 package org.example.consumer.repository;
 
-import org.example.consumer.model.TimeSeriesMessage;
+import org.example.consumer.model.TimeSeriesRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TimeseriesRepository extends JpaRepository<TimeSeriesMessage, Long> {
+public interface TimeseriesRepository extends JpaRepository<TimeSeriesRecord, Long> {
 }
