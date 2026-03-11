b6c352928c655500b8818a1f5c47ae2a1f0bd2d4
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2026-03-11 09:45:37 +0000

    refactor(TimeSeriesRecordService): remove unused methods

--- app/src/main/java/org/cotc/service/TimeSeriesRecordService.java
+++ app/src/main/java/org/cotc/service/TimeSeriesRecordService.java
@@ -1,11 +1,7 @@
 package org.cotc.service;
 
-import org.cotc.model.Group;
-import org.cotc.model.Producer;
 import org.cotc.model.TimeSeriesRecord;
 import org.cotc.utils.Translators;
-import org.cotc.libcotc.dto.ProducerDTO;
-import org.cotc.libcotc.dto.GroupDTO;
 import org.cotc.libcotc.dto.TimeSeriesRecordDTO;
 import org.cotc.repository.ProducerRepository;
 import org.cotc.repository.GroupRepository;
@@ -22,8 +18,6 @@ public class TimeSeriesRecordService {
 
     private static final Logger logger = LoggerFactory.getLogger(TimeSeriesRecordService.class);
 
-    private final GroupRepository groupRepository;
-    private final ProducerRepository producerRepository;
     private final TimeseriesRepository timeseriesRepository;
 
     @Autowired
@@ -33,27 +27,7 @@ public class TimeSeriesRecordService {
             TimeseriesRepository timeseriesRepository,
             Translators translators
     ) {
-        this.groupRepository = groupRepository;
-        this.producerRepository = producerRepository;
         this.timeseriesRepository = timeseriesRepository;
-    }
-
-    public List<GroupDTO> getAllStreams() {
-        logger.debug("getAllStreams - querying repository");
-        List<GroupDTO> streams = groupRepository.findAll().stream()
-                .map(Group::toDTO)
-                .toList();
-        logger.debug("getAllStreams - returning {} stream(s)", streams.size());
-        return streams;
-    }
-
-    public List<GroupDTO> getGroupHierarchy() {
-        logger.debug("getGroupHierarchy - querying repository");
-        List<Group> allGroups = groupRepository.findAll();
-        logger.debug("getGroupHierarchy - building DTO map from {} stream(s)", allGroups.size());
-        List<GroupDTO> roots = Translators.hierarchyFromGroups(allGroups);
-        logger.debug("getGroupHierarchy - returning {} root stream(s)", roots.size());
-        return roots;
     }
 
     public List<TimeSeriesRecordDTO> getRecordsByGroupId(String groupName) {
